3c09aff78d8c02faf250313a70052aa0b021f42b
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-07-17 21:13:42 +0800

    feat: add dashboard layout

---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import { Toaster } from "vue-sonner";
+
+const navigations = [
+  {
+    text: "Dashboard",
+    icon: "lucide:home",
+    link: "/app",
+    exact: true,
+  },
+  {
+    text: "User",
+    icon: "lucide:user",
+    link: "/app/user",
+    exact: false,
+  },
+];
+</script>
+
+<template>
+  <div>
+    <div class="flex min-h-screen">
+      <aside
+        class="h-screen sticky top-0 flex flex-col overflow-y-auto border-r-[1px] border-base-200 bg-base-100"
+      >
+        <!-- Header -->
+        <div class="flex justify-between p-2">
+          <!-- Logo -->
+          <a class="btn btn-ghost text-lg">
+            <img alt="Logo" src="/logo.svg" class="h-4" />
+          </a>
+
+          <a class="btn btn-ghost btn-circle text-lg">
+            <Icon name="lucide:settings" />
+          </a>
+        </div>
+
+        <!-- Body -->
+        <div class="flex flex-col border-y border-base-300 px-6 pt-4 grow">
+          <!-- Search input -->
+          <input class="input input-bordered" placeholder="Search..." />
+
+          <!-- Links -->
+          <div class="flex flex-col divide-y divide-base-300">
+            <UiNavList :navigations />
+
+            <ul class="menu px-0 py-4">
+              <li>
+                <a>
+                  <Icon name="logos:github-icon" />
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Icon name="logos:facebook" />
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Icon name="logos:youtube-icon" />
+                  Youtube
+                </a>
+              </li>
+            </ul>
+
+            <ul class="menu px-0 py-4">
+              <li>
+                <a>
+                  <Icon name="lucide:truck" />
+                  Deliver
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Icon name="lucide:shopping-bag" />
+                  Products
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Icon name="lucide:store" />
+                  Store
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Icon name="lucide:cherry" />
+                  Fruits
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- Footer -->
+        <div class="flex justify-between items-center p-2 gap-4">
+          <slot name="footer">
+            <a class="btn">
+              <img alt="Profile" src="/avatar.png" class="w-8 rounded-full" />
+
+              <div class="flex flex-col text-start">
+                <span class="font-bold">User name</span>
+                <span class="text-sm text-accent">user@email.com</span>
+              </div>
+            </a>
+
+            <NuxtLink class="btn btn-secondary btn-sm mr-3" title="Logout">
+              <Icon name="lucide:door-open" />
+            </NuxtLink>
+          </slot>
+        </div>
+      </aside>
+
+      <div class="flex flex-col grow">
+        <slot />
+      </div>
+    </div>
+
+    <Toaster closeButton position="top-right" />
+  </div>
+</template>
+
+<style></style>
