ef43c378323f5f6d5c83a717ba4f7ccaaca21ac6
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-07-17 21:13:32 +0800

    feat: add NavList & NavListItem component

---
+++ app/components/Ui/Nav/UiNavList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+export type UiNav = {
+  text: string;
+  icon: string;
+  link: string;
+  exact?: boolean;
+};
+
+const props = defineProps<{
+  navigations: UiNav[];
+}>();
+</script>
+
+<template>
+  <ul class="menu px-0 py-4">
+    <UiNavListItem v-for="nav in navigations" :key="nav.link" v-bind="nav" />
+  </ul>
+</template>
+
+<style></style>
---
+++ app/components/Ui/Nav/UiNavListItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { UiNav } from "./UiNavList.vue";
+
+const props = defineProps<UiNav>();
+</script>
+
+<template>
+  <li>
+    <NuxtLink
+      :to="link"
+      :exact="exact"
+      exact-active-class="active hover:!opacity-80"
+    >
+      <Icon :name="icon" />
+      {{ text }}
+    </NuxtLink>
+  </li>
+</template>
+
+<style></style>
