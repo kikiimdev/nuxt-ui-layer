451a711b45da4dfdc2275f92280498f3e30d743c
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-07-29 03:21:50 +0800

    feat: add UiCard

---
+++ app/components/Ui/Card/UiCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  subtitle?: string;
+  noPadding?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="card bg-base-100 border border-base-300">
+    <div class="card-body" :class="[noPadding ? 'p-0' : 'p-6']">
+      <h2 v-if="title" class="card-title text-2xl">
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </h2>
+      <p class="text-sm">
+        <slot name="default">
+          {{ subtitle }}
+        </slot>
+      </p>
+
+      <slot name="footer">
+        <div v-if="$slots['actions']" class="card-actions justify-start mt-2">
+          <slot name="actions" />
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style></style>
