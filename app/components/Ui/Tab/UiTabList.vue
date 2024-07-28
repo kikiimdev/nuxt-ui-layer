aa098d347d2f16382c4793110a835ec75d32dc07
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-07-29 03:22:03 +0800

    feat: add UiTab

---
+++ app/components/Ui/Tab/UiTabList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const modelValue = defineModel<any>();
+const updateValue = (newValue: any) => {
+  modelValue.value = newValue;
+};
+
+provide("activeTab", {
+  activeTab: modelValue,
+  changeTab: updateValue,
+});
+</script>
+
+<template>
+  <div class="flex">
+    <div
+      role="tablist"
+      class="tabs tabs-boxed bg-base-300 border border-base-200 gap-1"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ app/components/Ui/Tab/UiTabListItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value?: any;
+}>();
+
+const { activeTab, changeTab } = inject("activeTab") as any;
+
+const isActiveTab = computed(
+  () => !!props.value && props.value == unref(activeTab)
+);
+</script>
+
+<template>
+  <a
+    role="tab"
+    class="tab"
+    :class="[isActiveTab ? '!bg-base-100 font-medium' : 'text-base-content/65']"
+    @click="() => !!value && changeTab(value)"
+  >
+    <slot />
+  </a>
+</template>
+
+<style></style>
