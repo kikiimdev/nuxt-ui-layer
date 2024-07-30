6e35455d0df8ebe61139eaf0869fea3185e6c2f1
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-07-31 00:02:07 +0800

    feat: add UiPaginate

---
+++ app/components/Ui/Paginate/UiPaginate.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+  paginate: any;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <div class="text-sm">
+      Total: <strong>{{ paginate.total }}</strong>
+    </div>
+    <div class="join">
+      <button
+        class="join-item btn btn-sm bg-base-100"
+        :disabled="paginate.disabledPrevButton"
+        @click="paginate.prevPage()"
+      >
+        <Icon name="lucide:arrow-left" />
+      </button>
+      <button class="join-item btn btn-sm bg-base-100">
+        Page {{ paginate.page }} / {{ paginate.totalPage }}
+      </button>
+      <button
+        class="join-item btn btn-sm bg-base-100"
+        :disabled="paginate.disabledNextButton"
+        @click="paginate.nextPage()"
+      >
+        <Icon name="lucide:arrow-right" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style></style>
