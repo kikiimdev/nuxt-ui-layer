e2d9b7776b16141cbb0449443d60515f2c8a6645
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-07 14:32:51 +0800

    feat: add UiNavBottom

---
+++ app/components/Ui/Nav/Bottom/UiNavBottomItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { UiNav } from "./UiNavList.vue";
+const props = defineProps<UiNav>();
+</script>
+
+<template>
+  <button>
+    <Icon :name="icon" class="w-6 h-6" />
+    <span class="btm-nav-label">{{ text }}</span>
+  </button>
+</template>
+
+<style></style>
