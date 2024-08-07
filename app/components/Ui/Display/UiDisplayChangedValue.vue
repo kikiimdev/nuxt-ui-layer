cffcfc10cd8f75845e489e485a06ba22478443eb
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-08 01:20:18 +0800

    style: UiDisplayChangedValue

--- app/components/Ui/Display/UiDisplayChangedValue.vue
+++ app/components/Ui/Display/UiDisplayChangedValue.vue
@@ -21,32 +21,19 @@ const displayValue = (value: any) => {
       {{ isChanged ? displayValue(newValue) : displayValue(value) }}
     </div>
 
-    <div
+    <UiCard
+      no-padding
       tabindex="0"
       :class="[!isChanged && 'hidden']"
-      class="dropdown-content card card-compact z-[1] p-2 shadow"
+      class="dropdown-content card card-compact z-[1]"
     >
-      <UiCard no-padding>
-        <p class="p-2">
-          {{ displayValue(newValue) }}
-          <Icon name="lucide:chevron-right" />
-          {{ displayValue(value) }}
-        </p>
-      </UiCard>
-    </div>
+      <p class="p-2">
+        {{ displayValue(newValue) }}
+        <Icon name="lucide:chevron-right" />
+        {{ displayValue(value) }}
+      </p>
+    </UiCard>
   </div>
-
-  <!-- <div class="dropdown">
-  <div tabindex="0" role="button" class="btn m-1">Click</div>
-  <div
-    tabindex="0"
-    class="dropdown-content card card-compact bg-primary text-primary-content z-[1] w-64 p-2 shadow">
-    <div class="card-body">
-      <h3 class="card-title">Card title!</h3>
-      <p>you can use any element as a dropdown.</p>
-    </div>
-  </div>
-</div> -->
 </template>
 
 <style></style>
