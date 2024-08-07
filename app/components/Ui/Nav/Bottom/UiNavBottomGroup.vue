79d31cf6db2f1c902fbe8b2845c416a3fd709607
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-07 15:10:49 +0800

    style: update UiNavBottomGroup

--- app/components/Ui/Nav/Bottom/UiNavBottomGroup.vue
+++ app/components/Ui/Nav/Bottom/UiNavBottomGroup.vue
@@ -2,22 +2,28 @@
 import type { UiNav } from "../UiNavList.vue";
 const props = defineProps<UiNav>();
 const showDialog = ref(false);
+
+const isActive = computed(() =>
+  useRoute().matched.some((m) => m.path.startsWith(props.link))
+);
 </script>
 
 <template>
-  <button @click="showDialog = true">
+  <button :class="[isActive && 'active']" @click="showDialog = true">
     <Icon :name="icon" class="w-6 h-6" />
     <span class="btm-nav-label">{{ text }}</span>
   </button>
 
   <UiDialog v-model="showDialog" closeOnClickOutside boxClass="w-screen">
-    <ul>
+    <ul class="menu bg-base-200 rounded-box w-56">
+      <li class="menu-title">{{ text }}</li>
       <li v-for="children in childrens" :key="children.link">
         <NuxtLink
           :to="children.link"
           :exact="children.exact"
           exact-active-class="active"
         >
+          <Icon v-if="children.icon" :name="children.icon" />
           {{ children.text }}
         </NuxtLink>
       </li>
