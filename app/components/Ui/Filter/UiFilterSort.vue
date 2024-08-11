66c547c68caa6f6e3fd83b159107a5f80169303e
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-08-12 04:35:53 +0800

    feat: add UiFilterSort

---
+++ app/components/Ui/Filter/UiFilterSort.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const props = defineProps<{
+  items: any;
+}>();
+
+const modelValue = defineModel<any>();
+const direction = defineModel<"asc" | "desc">("direction", {
+  required: false,
+  default: "asc",
+});
+</script>
+
+<template>
+  <UiFilterSelect
+    v-model="modelValue"
+    :items
+    label="Urutkan"
+    :icon="
+      direction === 'asc' ? 'lucide:arrow-down-a-z' : 'lucide:arrow-up-a-z'
+    "
+    remove-show-all
+  >
+    <template #list-prepend>
+      <div class="join min-w-[162px]">
+        <input
+          type="radio"
+          name="direction-buttons"
+          class="btn btn-sm join-item"
+          aria-label="Menurun"
+          value="asc"
+          v-model="direction"
+        />
+        <input
+          type="radio"
+          name="direction-buttons"
+          class="btn btn-sm join-item"
+          aria-label="Meningkat"
+          value="desc"
+          v-model="direction"
+        />
+      </div>
+      <div class="divider -my-0" />
+    </template>
+  </UiFilterSelect>
+</template>
+
+<style></style>
